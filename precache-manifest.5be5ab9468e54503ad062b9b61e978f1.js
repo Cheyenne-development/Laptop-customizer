a104396c1e45edcc087a0bcce0a28d39f5dbdabd
Cheyenne McMahon <orlandu8@gmail.com>
2020-12-07 03:00:09 -0600

    Updates

---
+++ precache-manifest.5be5ab9468e54503ad062b9b61e978f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a993378bd8126aca30bf722d092c2f4",
+    "url": "/laptop-customizer/index.html"
+  },
+  {
+    "revision": "e8a78b195bba1cadabf7",
+    "url": "/laptop-customizer/static/css/main.42a420bb.chunk.css"
+  },
+  {
+    "revision": "df049e72868648be55c4",
+    "url": "/laptop-customizer/static/js/2.487cfc0f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/laptop-customizer/static/js/2.487cfc0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a78b195bba1cadabf7",
+    "url": "/laptop-customizer/static/js/main.6a50ebe5.chunk.js"
+  },
+  {
+    "revision": "e352c3795ce0317e3f2b",
+    "url": "/laptop-customizer/static/js/runtime-main.4f50e2e6.js"
+  }
+]);
